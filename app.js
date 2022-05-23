bc04e5548c95b27d3836d2295aa54ee3e57dff64
regex <aditya777regex@gmail.com>
2022-05-23 15:34:10 +0530

    day-1 #1

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+//intialize express
+const app = express();
+//intialize body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+//set view engine
+app.set('view engine', 'ejs');
+//set public folder
+app.use(express.static('public'));
+//APP PORT
+const port = 3000 || process.env.PORT;
+
+//mongoose connection
+mongoose.connect('mongodb://localhost:27017/accounterDB', {useNewUrlParser: true},function(err){
+    if(err){
+        console.log("❗ | "+err.red);
+    }else{
+        console.log("⚙ | Connected to Database Succesfully.".green);
+    }
+});
+
+
+//routes
+app.get('/', function(res,res){
+    res.render('index');
+})
+
+
+app.listen(port, function(){
+    console.log("🚀 | Server Started at port "+port.toString().green);
+})
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./views/**/*.{html,ejs}"],
+    theme: {
+      extend: {
+          fontFamily: {
+              "grape-nuts": "Grape Nuts, cursive",
+              "montserrat": "Montserrat, sans-serif"
+          }
+      },
+    },
+    plugins: [],
+  }
