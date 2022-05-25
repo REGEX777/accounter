6a325f245bc4711884e7411285ad85c14d1daa68
regex <aditya777regex@gmail.com>
2022-05-25 15:10:45 +0530

    day-3 #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
         "slight-gray": "#e4e4e4"
       },
       extend: {
+          animation:  {
+              "bounce-right": "bounce-right 1s ease-in-out",
+          },
           fontFamily: {
               "grape-nuts": "Grape Nuts, cursive",
               "montserrat": "Montserrat, sans-serif"
